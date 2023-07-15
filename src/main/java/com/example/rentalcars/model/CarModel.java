990d1a6b78e75ffcc102a712fe9d3f0c07f7122a
wnusiek <120226590+wnusiek@users.noreply.github.com>
2023-07-15 11:59:08 +0200

    Create CarModel.java

---
+++ src/main/java/com/example/rentalcars/model/CarModel.java
@@ -0,0 +1,52 @@
+package com.example.rentalcars.model;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.math.BigDecimal;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+@Entity
+public class CarModel {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "mark")
+    private String mark;
+
+    @Column(name = "model")
+    private String model;
+
+    @Column(name = "price")
+    private BigDecimal price;
+
+    @Column(name = "bail")
+    private String bail;
+
+    @Column(name = "body")
+    private String body;
+
+    @Column(name = "gearbox")
+    private String gearbox;
+
+    @Column(name = "number_of_seats")
+    private Integer numberOfSeats;
+
+    @Column(name = "number_of_doors")
+    private Integer numberOfDoors;
+
+    @Column(name = "fuel_type")
+    private String fuelType;
+
+    @Column(name = "trunk")
+    private String trunk;
+
+}
