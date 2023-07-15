4a32959c1d0e81bda39d45708b01e1f90420d3c6
Dominik Swiesciak <dominikswiesciak@gmail.com>
2023-07-15 13:20:03 +0200

    delete availables

--- src/main/java/com/example/rentalcars/repository/AvailabilityRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.rentalcars.repository;
-
-import com.example.rentalcars.model.AvailabilityModel;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface AvailabilityRepository extends JpaRepository<AvailabilityModel, Long> {
-}
