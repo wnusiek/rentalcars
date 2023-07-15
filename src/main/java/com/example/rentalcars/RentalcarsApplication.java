514bf23afb2b26155115922dee0c8cf31ffcb848
wnusiek <120226590+wnusiek@users.noreply.github.com>
2023-07-15 10:11:29 +0200

    project created

---
+++ src/main/java/com/example/rentalcars/RentalcarsApplication.java
@@ -0,0 +1,13 @@
+package com.example.rentalcars;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RentalcarsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RentalcarsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/rentalcars/RentalcarsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.rentalcars;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RentalcarsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
