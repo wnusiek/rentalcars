3c8e61a059e95f59371efb27aab5cc048487b977
Dominik Swiesciak <dominikswiesciak@gmail.com>
2023-07-15 11:52:20 +0200

    Create AvilabilityModel.java

---
+++ src/main/java/com/example/rentalcars/model/AvilabilityModel.java
@@ -0,0 +1,27 @@
+package com.example.rentalcars.model;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+@Entity
+public class AvilabilityModel {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "car_id")
+    private Long carId;
+
+    @Column(name = "availability")
+    private Boolean availability;
+}
+
+
