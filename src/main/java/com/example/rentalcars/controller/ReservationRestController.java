c24c79956ec9b5f45da15045e7df40919b750929
wnusiek <120226590+wnusiek@users.noreply.github.com>
2023-07-16 21:13:17 +0200

    dodana metoda add
    dodana metoda add w ReservationRestController
    zakomentowane metody w ReservationController

--- src/main/java/com/example/rentalcars/controller/ReservationRestController.java
+++ src/main/java/com/example/rentalcars/controller/ReservationRestController.java
@@ -4,20 +4,27 @@ import com.example.rentalcars.model.ReservationModel;
 import com.example.rentalcars.service.ReservationService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
-@RestController
-@RequestMapping("/reservation")
 @RequiredArgsConstructor
+@RestController
+//@RequestMapping("/reservation")
 public class ReservationRestController {
 
     private final ReservationService reservationService;
 
-    @GetMapping
+    @GetMapping("/reservation")
     public List<ReservationModel> get(){
         return reservationService.getReservationList();
     }
+
+    @PostMapping("/addReservation")
+    public void add(ReservationModel reservation){
+        reservationService.addReservation(reservation);
+    }
 }
+
