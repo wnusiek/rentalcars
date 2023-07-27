6a5f3b52ca2f80a3ebf4eed3273fd27409a8ca68
Dominik Swiesciak <dominikswiesciak@gmail.com>
2023-07-27 18:39:54 +0200

    Security Vaadin
    dodane logowanie i przycisk logout
    user hasło userpass

--- src/main/java/com/example/rentalcars/views/main/MainLayout.java
+++ src/main/java/com/example/rentalcars/views/main/MainLayout.java
@@ -1,7 +1,9 @@
 package com.example.rentalcars.views.main;
 
+import com.example.rentalcars.security.SecurityService;
 import com.vaadin.flow.component.applayout.AppLayout;
 import com.vaadin.flow.component.applayout.DrawerToggle;
+import com.vaadin.flow.component.button.Button;
 import com.vaadin.flow.component.html.H1;
 import com.vaadin.flow.component.orderedlayout.FlexComponent;
 import com.vaadin.flow.component.orderedlayout.HorizontalLayout;
@@ -9,12 +11,14 @@ import com.vaadin.flow.component.orderedlayout.VerticalLayout;
 import com.vaadin.flow.router.HighlightConditions;
 import com.vaadin.flow.router.Route;
 import com.vaadin.flow.router.RouterLink;
+import jakarta.annotation.security.PermitAll;
 
 import javax.swing.text.html.ListView;
-
+@PermitAll
 public class MainLayout extends AppLayout {
-
-    public MainLayout() {
+    private SecurityService securityService;
+    public MainLayout(SecurityService securityService) {
+        this.securityService = securityService;
         createHeader();
         createDrawer();
 
@@ -23,7 +27,9 @@ public class MainLayout extends AppLayout {
         H1 logo = new H1("Wypożyczalnia Gruz-rental");
         logo.addClassNames("text-l", "m-m");
 
-        HorizontalLayout header = new HorizontalLayout(new DrawerToggle(), logo);
+        Button logOut = new Button("Log out", e -> securityService.logout());
+
+        HorizontalLayout header = new HorizontalLayout(new DrawerToggle(), logo, logOut);
         header.setDefaultVerticalComponentAlignment(FlexComponent.Alignment.CENTER);
         header.expand(logo);
         header.setWidthFull();
