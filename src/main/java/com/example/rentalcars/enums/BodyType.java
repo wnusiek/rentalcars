b88b43f3ae861a2633171f52026cebe6e79fed71
wnusiek <120226590+wnusiek@users.noreply.github.com>
2023-07-28 23:22:47 +0200

    Update BodyType.java

--- src/main/java/com/example/rentalcars/enums/BodyType.java
+++ src/main/java/com/example/rentalcars/enums/BodyType.java
@@ -1,12 +1,26 @@
 package com.example.rentalcars.enums;
 
+import lombok.Getter;
+import lombok.Setter;
+
 public enum BodyType {
-    HATCHBACK,
-    WAGON,
-    SEDAN,
-    COUPE,
-    SUV,
-    CONVERTIBLE,
-    PICKUP,
-    MINIVAN
+    HATCHBACK("Hatchback"),
+    WAGON("Wagon"),
+    SEDAN("Sedan"),
+    COUPE("Coupe"),
+    SUV("Suv"),
+    CONVERTIBLE("Convertible"),
+    PICKUP("Pickup"),
+    MINIVAN("Minivan");
+
+    private String name;
+
+    private BodyType(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public String toString(){
+        return name;
+    }
 }
