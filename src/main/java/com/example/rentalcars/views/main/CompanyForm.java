ec7c3e458543f3bde11d88e187c4fc85fda4c434
wnusiek <120226590+wnusiek@users.noreply.github.com>
2023-07-30 02:35:36 +0200

    Company

---
+++ src/main/java/com/example/rentalcars/views/main/CompanyForm.java
@@ -0,0 +1,87 @@
+package com.example.rentalcars.views.main;
+
+import com.example.rentalcars.model.CompanyModel;
+import com.vaadin.flow.component.Component;
+import com.vaadin.flow.component.ComponentEvent;
+import com.vaadin.flow.component.ComponentEventListener;
+import com.vaadin.flow.component.button.Button;
+import com.vaadin.flow.component.button.ButtonVariant;
+import com.vaadin.flow.component.orderedlayout.HorizontalLayout;
+import com.vaadin.flow.component.orderedlayout.VerticalLayout;
+import com.vaadin.flow.component.textfield.TextField;
+import com.vaadin.flow.data.binder.BeanValidationBinder;
+import com.vaadin.flow.data.binder.Binder;
+import com.vaadin.flow.data.binder.ValidationException;
+import com.vaadin.flow.shared.Registration;
+
+public class CompanyForm extends VerticalLayout {
+    Binder<CompanyModel> binder = new BeanValidationBinder<>(CompanyModel.class);
+    TextField companyName = new TextField("Company name");
+    TextField companyDomainURL = new TextField("Company domain URL");
+    TextField companyAddress = new TextField("Company address");
+    TextField companyOwner = new TextField("Company owner");
+    TextField companyLogotype = new TextField("Company Logotype");
+
+    Button save = new Button("Save");
+    private CompanyModel companyModel;
+
+    public CompanyForm(){
+        binder.bindInstanceFields(this);
+        addClassName("company-form");
+        add(
+                companyName,
+                companyDomainURL,
+                companyAddress,
+                companyOwner,
+                companyLogotype,
+                createButtonLayout()
+        );
+    }
+    public void setCompany(CompanyModel companyModel){
+        this.companyModel = companyModel;
+        binder.readBean(companyModel);
+    }
+
+    private Component createButtonLayout() {
+        save.addThemeVariants(ButtonVariant.LUMO_PRIMARY);
+        save.addClickListener(event -> validateAndSave());
+        return new HorizontalLayout(save);
+    }
+    private void validateAndSave() {
+        try {
+            binder.writeBean(companyModel);
+            fireEvent(new SaveEvent(this, companyModel));
+        } catch (ValidationException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static abstract class CompanyFormEvent extends ComponentEvent<CompanyForm> {
+        private CompanyModel companyModel;
+        protected CompanyFormEvent(CompanyForm source, CompanyModel companyModel) {
+            super(source, false);
+            this.companyModel = companyModel;
+        }
+        public CompanyModel getCompany() {
+            return companyModel;
+        }
+    }
+    public static class SaveEvent extends CompanyForm.CompanyFormEvent {
+        SaveEvent(CompanyForm source, CompanyModel companyModel) {
+            super(source, companyModel);
+        }
+    }
+
+    public static class CloseEvent extends CompanyForm.CompanyFormEvent {
+        CloseEvent(CompanyForm source) {
+            super(source, null);
+        }
+    }
+
+    public Registration addSaveListener(ComponentEventListener<CompanyForm.SaveEvent> listener) {
+        return addListener(CompanyForm.SaveEvent.class, listener);
+    }
+    public Registration addCloseListener(ComponentEventListener<CompanyForm.CloseEvent> listener) {
+        return addListener(CompanyForm.CloseEvent.class, listener);
+    }
+}
--- src/main/java/com/example/rentalcars/views/main/CompanyView.java
+++ src/main/java/com/example/rentalcars/views/main/CompanyView.java
@@ -1,8 +1,11 @@
 package com.example.rentalcars.views.main;
 
 import com.example.rentalcars.model.CompanyModel;
+import com.example.rentalcars.model.EmployeeModel;
 import com.example.rentalcars.service.CompanyService;
+import com.vaadin.flow.component.Component;
 import com.vaadin.flow.component.grid.Grid;
+import com.vaadin.flow.component.orderedlayout.HorizontalLayout;
 import com.vaadin.flow.component.orderedlayout.VerticalLayout;
 import com.vaadin.flow.router.PageTitle;
 import com.vaadin.flow.router.Route;
@@ -13,16 +16,48 @@ public class CompanyView extends VerticalLayout {
 
     private final CompanyService companyService;
     Grid<CompanyModel> grid = new Grid<>(CompanyModel.class);
+    CompanyForm form = new CompanyForm();
 
     public CompanyView(CompanyService companyService) {
         this.companyService = companyService;
         addClassName("company-view");
         setSizeFull();
         configureGrid();
-        add(grid);
-        getCompanyInfo();
+        configureForm();
+        add(getContent());
+        updateCompanyInfo();
+        closeEditor();
+
     }
-    private void getCompanyInfo() {
+    private void closeEditor() {
+        form.setCompany(null);
+        form.setVisible(false);
+        removeClassName("editing");
+    }
+
+    private Component getContent() {
+        HorizontalLayout content = new HorizontalLayout(grid, form);
+        content.setFlexGrow(2,grid);
+        content.setFlexGrow(1, form);
+        content.addClassName("content");
+        content.setSizeFull();
+        return content;
+    }
+
+    private void configureForm() {
+        form = new CompanyForm();
+        form.setWidth("25em");
+
+        form.addSaveListener(this::saveCompany);
+        form.addCloseListener(event -> closeEditor());
+    }
+
+    private void saveCompany(CompanyForm.SaveEvent event){
+        companyService.updateCompany(event.getCompany());
+        updateCompanyInfo();
+        closeEditor();
+    }
+    private void updateCompanyInfo() {
         grid.setItems(companyService.getCompanyModelList());
     }
     private void configureGrid() {
@@ -30,6 +65,16 @@ public class CompanyView extends VerticalLayout {
         grid.setSizeFull();
         grid.setColumns("companyName", "companyDomainURL", "companyAddress", "companyOwner", "companyLogotype");
         grid.getColumns().forEach(col -> col.setAutoWidth(true));
+        grid.asSingleSelect().addValueChangeListener(event -> editCompany(event.getValue()));
+    }
+    private void editCompany(CompanyModel companyModel) {
+        if(companyModel == null){
+            closeEditor();
+        }else {
+            form.setCompany(companyModel);
+            form.setVisible(true);
+            addClassName("editing");
+        }
     }
 
 }
