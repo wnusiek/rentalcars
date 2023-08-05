11b0dc2b1d2789b2cdac27685e90cb04d04925ec
wnusiek <120226590+wnusiek@users.noreply.github.com>
2023-08-05 19:26:08 +0200

    dodana metoda pobierania nazwy zalogowanego usera

--- src/main/java/com/example/rentalcars/service/UserService.java
+++ src/main/java/com/example/rentalcars/service/UserService.java
@@ -3,14 +3,13 @@ package com.example.rentalcars.service;
 import com.example.rentalcars.model.UserModel;
 import com.example.rentalcars.repository.RoleRepository;
 import com.example.rentalcars.repository.UserRepository;
-import jdk.jshell.spi.ExecutionControl;
 import lombok.RequiredArgsConstructor;
-import org.apache.catalina.User;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
-import java.util.stream.Collectors;
 
 @Service
 @RequiredArgsConstructor
@@ -50,7 +49,7 @@ public class UserService {
         return false;
     }
 
-    public UserModel findByNameModel(String name) {
+    public UserModel findUserByNameModel(String name) {
         for (UserModel user : getUserList()) {
             if (user.getName().equals(name)) {
                 return user;
@@ -69,6 +68,13 @@ public class UserService {
             user.setActive(true);
             userRepository.save(user);
 
+    }
+
+
+    public String getNameOfLoggedUser(){
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        String nameOfLoggedUser = authentication.getName();
+        return nameOfLoggedUser;
     }
 
     public boolean checkIfUserExists(UserModel userModel) {
