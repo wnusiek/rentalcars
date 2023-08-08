38cc1a3fa4817dfe6717b71d28b8f77d5462ab61
Paweł Wnuk <pawel.jan.wnuk@gmail.com>
2023-08-08 11:30:17 +0200

    poprawione id

--- src/main/java/com/example/rentalcars/service/CompanyService.java
+++ src/main/java/com/example/rentalcars/service/CompanyService.java
@@ -22,6 +22,6 @@ public class CompanyService {
     }
 
     public String getCompanyName(){
-        return getCompanyModelList().get(1).getCompanyName();
+        return getCompanyModelList().get(0).getCompanyName();
     }
 }
