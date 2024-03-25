08e4c3148048d4b6d5999f7219c127c67b65c5e6
wnusiek <120226590+wnusiek@users.noreply.github.com>
2024-03-26 00:18:56 +0100

    Update UserServiceTests.java

--- src/test/java/com/example/rentalcars/service/UserServiceTests.java
+++ src/test/java/com/example/rentalcars/service/UserServiceTests.java
@@ -1,21 +1,27 @@
 package com.example.rentalcars.service;
 
-import com.example.rentalcars.model.EmployeeModel;
 import com.example.rentalcars.model.RoleModel;
 import com.example.rentalcars.model.UserModel;
 import com.example.rentalcars.repository.RoleRepository;
 import com.example.rentalcars.repository.UserRepository;
+
+import static org.assertj.core.api.Assertions.assertThat;
 import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.BDDMockito.given;
+import static org.mockito.BDDMockito.willDoNothing;
+import static org.mockito.Mockito.*;
+
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 
+
 import java.util.List;
 
-import static org.mockito.BDDMockito.given;
 
 @ExtendWith(MockitoExtension.class)
 public class UserServiceTests {
@@ -41,6 +47,16 @@ public class UserServiceTests {
     public void givenUserList_whenGetAllUsers_thenReturnUsersList(){
 
         // given
-//        given(userRepository.findAll()).willReturn(List.of(user,user1));
+        var role1 = roleRepository.save(new RoleModel(1l, "ADMIN"));
+        UserModel userModel1 = new UserModel(2L, "JimiHendrix", "jimi123", "jhendrix@gmail.com", true, role1);
+        given(userRepository.findAll()).willReturn(List.of(userModel,userModel1));
+
+        // when
+        List<UserModel> userModelList = userService.getUserList();
+
+        // then
+        assertThat(userModelList).isNotNull();
+        assertThat(userModelList.size()).isEqualTo(2);
+
     }
 }
