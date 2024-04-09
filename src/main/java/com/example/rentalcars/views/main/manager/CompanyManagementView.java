427f11d45aabe2d648f258a5366ab8a242c6e59a
wnusiek <120226590+wnusiek@users.noreply.github.com>
2024-04-09 23:37:49 +0200

    Zmiana nazwy klasy widoku

--- src/main/java/com/example/rentalcars/views/main/manager/CompanyManagementView.java
+++ src/main/java/com/example/rentalcars/views/main/manager/CompanyManagementView.java
@@ -16,13 +16,13 @@ import org.springframework.security.access.annotation.Secured;
 @PageTitle("Konfiguracja")
 @Secured({"ROLE_MANAGER", "ROLE_ADMIN"})
 @RolesAllowed({"ROLE_MANAGER", "ROLE_ADMIN"})
-public class CompanyView extends VerticalLayout {
+public class CompanyManagementView extends VerticalLayout {
 
     private final CompanyService companyService;
     Grid<CompanyModel> grid = new Grid<>(CompanyModel.class);
-    CompanyForm form = new CompanyForm();
+    CompanyManagementForm form = new CompanyManagementForm();
 
-    public CompanyView(CompanyService companyService) {
+    public CompanyManagementView(CompanyService companyService) {
         this.companyService = companyService;
         addClassName("company-view");
         setSizeFull();
@@ -49,14 +49,14 @@ public class CompanyView extends VerticalLayout {
     }
 
     private void configureForm() {
-        form = new CompanyForm();
+        form = new CompanyManagementForm();
         form.setWidth("25em");
 
         form.addSaveListener(this::saveCompany);
         form.addCloseListener(event -> closeEditor());
     }
 
-    private void saveCompany(CompanyForm.SaveEvent event){
+    private void saveCompany(CompanyManagementForm.SaveEvent event){
         companyService.updateCompany(event.getCompany());
         updateCompanyInfo();
         closeEditor();
