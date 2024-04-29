970cb35018ebd7133f0acc0fab1f2eb162a1ef5e
wnusiek <120226590+wnusiek@users.noreply.github.com>
2024-04-29 15:22:16 +0200

    Update ReservationRepository.java
    dodanie zapytania filtrującego rezerwacje po nazwisku klienta i dacie rezerwacji

--- src/main/java/com/example/rentalcars/repository/ReservationRepository.java
+++ src/main/java/com/example/rentalcars/repository/ReservationRepository.java
@@ -6,11 +6,14 @@ import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
+import java.time.LocalDate;
 import java.util.List;
 
 @Repository
 public interface ReservationRepository extends JpaRepository<ReservationModel, Long > {
-    @Query("select r from ReservationModel r " +
-            "where lower(r.customer.lastName) like lower(concat('%', :searchTerm, '%'))")
+    @Query("select r from ReservationModel r where lower(r.customer.lastName) like lower(concat('%', :searchTerm, '%'))")
     List<ReservationModel> search(@Param("searchTerm") String searchTerm);
+
+    @Query("select r from ReservationModel r where lower(r.customer.lastName) like lower(concat('%', :searchTerm, '%')) and r.dateFrom = :searchDate")
+    List<ReservationModel> findByReservationDateFrom(@Param("searchTerm") String searchTerm, @Param("searchDate") LocalDate date);
 }
