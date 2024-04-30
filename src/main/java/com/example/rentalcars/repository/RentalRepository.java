2180d54dfd2fa0200036cbeed1fe5eb1cf222110
wnusiek <120226590+wnusiek@users.noreply.github.com>
2024-04-30 22:49:03 +0200

    Update RentalRepository.java
    Dodana metoda wyszukiwania z filtrami w RentalRepository

--- src/main/java/com/example/rentalcars/repository/RentalRepository.java
+++ src/main/java/com/example/rentalcars/repository/RentalRepository.java
@@ -1,5 +1,8 @@
 package com.example.rentalcars.repository;
 
+import com.example.rentalcars.enums.ReservationStatus;
+import com.example.rentalcars.model.CustomerModel;
+import com.example.rentalcars.model.DepartmentModel;
 import com.example.rentalcars.model.RentalModel;
 import com.example.rentalcars.model.ReservationModel;
 import org.springframework.data.jpa.repository.JpaRepository;
@@ -7,6 +10,7 @@ import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
+import java.time.LocalDate;
 import java.util.List;
 import java.util.Optional;
 
@@ -17,4 +21,16 @@ public interface RentalRepository extends JpaRepository<RentalModel, Long> {
     @Query("select r from RentalModel r " +
             "where lower(r.reservation.customer.lastName) like lower(concat('%', :searchTerm, '%'))")
     List<RentalModel> search(@Param("searchTerm") String searchTerm);
+
+    @Query("select r from RentalModel r " +
+            "where (:searchTerm is null or lower(r.reservation.customer.lastName) like lower(concat('%', :searchTerm, '%'))" +
+            "or lower(r.reservation.customer.firstName) like lower(concat('%', :searchTerm, '%')))" +
+            "and (:searchDate is null or r.reservation.dateFrom = :searchDate)" +
+            "and (:searchDepartment is null or r.reservation.receptionVenue = :searchDepartment)" +
+            "and (:searchReservationStatus is null or r.reservation.reservationStatus = :searchReservationStatus)")
+    List<RentalModel> findWithFilters(
+            @Param("searchTerm") CustomerModel customer,
+            @Param("searchDate") LocalDate date,
+            @Param("searchDepartment") DepartmentModel receptionVenue,
+            @Param("searchReservationStatus") ReservationStatus reservationStatus);
 }
