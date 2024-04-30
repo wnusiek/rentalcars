18d2b0ea2a24330bc2b14d6b67d852b5189b727b
wnusiek <120226590+wnusiek@users.noreply.github.com>
2024-04-30 22:49:43 +0200

    Update ReturnRepository.java
    Dodana metoda wyszukiwania z filtrami w ReturnRepository

--- src/main/java/com/example/rentalcars/repository/ReturnRepository.java
+++ src/main/java/com/example/rentalcars/repository/ReturnRepository.java
@@ -1,11 +1,15 @@
 package com.example.rentalcars.repository;
 
+import com.example.rentalcars.enums.ReservationStatus;
+import com.example.rentalcars.model.CustomerModel;
+import com.example.rentalcars.model.DepartmentModel;
 import com.example.rentalcars.model.ReservationModel;
 import com.example.rentalcars.model.ReturnModel;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
+import java.time.LocalDate;
 import java.util.List;
 import java.util.Optional;
 
@@ -15,4 +19,16 @@ public interface ReturnRepository extends JpaRepository<ReturnModel, Long> {
     @Query("select r from ReturnModel r " +
             "where lower(r.reservation.customer.lastName) like lower(concat('%', :searchTerm, '%'))")
     List<ReturnModel> search(@Param("searchTerm") String searchTerm);
+
+    @Query("select r from ReturnModel r " +
+            "where (:searchTerm is null or lower(r.reservation.customer.lastName) like lower(concat('%', :searchTerm, '%'))" +
+            "or lower(r.reservation.customer.firstName) like lower(concat('%', :searchTerm, '%')))" +
+            "and (:searchDate is null or r.reservation.dateFrom = :searchDate)" +
+            "and (:searchDepartment is null or r.reservation.receptionVenue = :searchDepartment)" +
+            "and (:searchReservationStatus is null or r.reservation.reservationStatus = :searchReservationStatus)")
+    List<ReturnModel> findWithFilters(
+            @Param("searchTerm") CustomerModel customer,
+            @Param("searchDate") LocalDate date,
+            @Param("searchDepartment") DepartmentModel receptionVenue,
+            @Param("searchReservationStatus") ReservationStatus reservationStatus);
 }
