da634d5ba851f1b5bab2cde5dc83dbc0202c0c0c
wnusiek <120226590+wnusiek@users.noreply.github.com>
2024-05-03 21:35:44 +0200

    Update ReturnView.java
    - Dodanie pola "Dopłata" i logiki dla ręcznego wprowadzanie dodatkowych kosztów (mandaty, uszkodzenia itd.)
    - Jeśli przekroczony został termin zwrotu to automatyczne dodanie (100 * liczba dni po  terminie) do całkowitego kosztu

--- src/main/java/com/example/rentalcars/views/main/employee/ReturnView.java
+++ src/main/java/com/example/rentalcars/views/main/employee/ReturnView.java
@@ -13,16 +13,21 @@ import com.vaadin.flow.component.button.Button;
 import com.vaadin.flow.component.combobox.ComboBox;
 import com.vaadin.flow.component.datepicker.DatePicker;
 import com.vaadin.flow.component.grid.Grid;
+import com.vaadin.flow.component.html.Div;
 import com.vaadin.flow.component.html.Span;
 import com.vaadin.flow.component.notification.Notification;
 import com.vaadin.flow.component.orderedlayout.HorizontalLayout;
 import com.vaadin.flow.component.orderedlayout.VerticalLayout;
+import com.vaadin.flow.component.textfield.BigDecimalField;
+import com.vaadin.flow.component.textfield.NumberField;
 import com.vaadin.flow.component.textfield.TextField;
 import com.vaadin.flow.router.PageTitle;
 import com.vaadin.flow.router.Route;
 import jakarta.annotation.security.RolesAllowed;
 import org.springframework.security.access.annotation.Secured;
 
+import java.math.BigDecimal;
+import java.time.Duration;
 import java.time.LocalDate;
 
 @Route(value = "returnView", layout = MainLayout.class)
@@ -42,6 +47,7 @@ public class ReturnView extends VerticalLayout {
     DatePicker dateOfReturn = new DatePicker("Data zwrotu");
     Button returnACarButton = new Button("Zwróć");
     TextField comments = new TextField("Komentarz");
+    BigDecimalField supplement = new BigDecimalField("Dopłata");
     ComboBox<DepartmentModel> returnVenue = new ComboBox<>("Oddział zwrotu");
     ReturnForm form;
     RentalModel employeeChoice;
@@ -89,6 +95,7 @@ public class ReturnView extends VerticalLayout {
         grid.setSizeFull();
         grid.addColumn("reservation.car.mark").setHeader("Marka");
         grid.addColumn("reservation.car.model").setHeader("Model");
+        grid.addColumn("dateOfRental").setHeader("Wypożyczenie od");
         grid.addColumn("reservation.dateFrom").setHeader("Rezerwacja od");
         grid.addColumn("reservation.dateTo" ).setHeader("Rezerwacja do");
         grid.addColumn("employee.firstName").setHeader("Imię pracownika");
@@ -96,7 +103,6 @@ public class ReturnView extends VerticalLayout {
         grid.addColumn("reservation.customer.firstName").setHeader("Imię klienta");
         grid.addColumn("reservation.customer.lastName").setHeader("Nazwisko klienta");
         grid.addColumn("reservation.price").setHeader("Cena");
-        grid.addColumn("dateOfRental").setHeader("Wypożyczenie od");
         grid.addColumn("comments").setHeader("Komentarz");
         grid.getColumns().forEach(col -> col.setAutoWidth(true));
         grid.asSingleSelect().addValueChangeListener(event -> saveEmployeeChoice(event.getValue()));
@@ -123,8 +129,13 @@ public class ReturnView extends VerticalLayout {
                         employeeService.getEmployeeByUserName(
                                 userService.getNameOfLoggedUser())));
         returnVenue.addValueChangeListener(e -> updateRentalList());
+        supplement.setPlaceholder("Wprowadź koszty");
+        supplement.setWidth("200px");
+        Div plnPrefix = new Div();
+        plnPrefix.setText("PLN");
+        supplement.setPrefixComponent(plnPrefix);
         HorizontalLayout toolbar = new HorizontalLayout();
-        toolbar.add(dateOfReturn, comments, returnACarButton);
+        toolbar.add(dateOfReturn, comments, supplement, returnACarButton);
         toolbar.addAndExpand(new Span());
         toolbar.add(returnVenue);
         toolbar.addClassName("toolbar");
@@ -148,6 +159,7 @@ public class ReturnView extends VerticalLayout {
         returnModel.setReservation(employeeChoice.getReservation());
         returnModel.setDateOfReturn(dateOfReturn.getValue());
         returnModel.setComments(comments.getValue());
+        returnModel.setSupplement(supplement.getValue());
         editReturn(returnModel);
     }
 
@@ -175,11 +187,25 @@ public class ReturnView extends VerticalLayout {
         Long carId = reservationModel.getCar().getId();
         Long returnVenueId = reservationModel.getReturnVenue().getId();
         Long reservationId = reservationModel.getId();
+        BigDecimal afterDeadlineFee = BigDecimal.valueOf(0);
         Boolean carReturnedAfterDeadline = returnModel.getDateOfReturn().isAfter(reservationModel.getDateTo());
         if(carReturnedAfterDeadline){
-            Notification.show("Zwrot po terminie!").setPosition(Notification.Position.MIDDLE);
+            Duration difference = Duration.between(returnModel.getDateOfReturn(), reservationModel.getDateTo());
+            afterDeadlineFee = BigDecimal.valueOf(100*difference.toDays());
+            Notification.show("Zwrot po terminie! Dodatkowa opłata: " + afterDeadlineFee).setPosition(Notification.Position.MIDDLE);
         }
-        returnModel.setTotalCost(reservationModel.getPrice());
+        if (supplement.getValue() == null){
+            returnModel.setSupplement(BigDecimal.valueOf(0));
+        } else {
+            // Brakuje sprawdzenia czy mniejsze od zera!!!
+            returnModel.setSupplement(supplement.getValue());
+            Notification.show("Dopłata: " + supplement.getValue()).setPosition(Notification.Position.MIDDLE);
+        }
+        Notification.show("Total cost: " + returnModel.getTotalCost()).setPosition(Notification.Position.MIDDLE);
+        returnModel.setTotalCost(reservationModel.getPrice()
+                .add(returnModel.getSupplement())
+                .add(afterDeadlineFee));
+        Notification.show("Total cost: " + returnModel.getTotalCost()).setPosition(Notification.Position.MIDDLE);
         returnService.addReturn(returnModel);
         departmentService.addCarToDepartment(carId, returnVenueId);
         carService.setCarStatus(carId, CarStatus.AVAILABLE);
