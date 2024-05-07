1d8065e2b6d29c00a9254a8e9a8e0b85a41ce96a
wnusiek <120226590+wnusiek@users.noreply.github.com>
2024-05-08 00:08:31 +0200

    Update CompanyService.java
    Dodane sprawdzenie czy lista nie jest pusta

--- src/main/java/com/example/rentalcars/service/CompanyService.java
+++ src/main/java/com/example/rentalcars/service/CompanyService.java
@@ -22,6 +22,6 @@ public class CompanyService {
     }
 
     public String getCompanyName() {
-        return getCompanyModelList().get(0).getCompanyName();
+        return getCompanyModelList().isEmpty() ? "" : getCompanyModelList().get(0).getCompanyName();
     }
 }
