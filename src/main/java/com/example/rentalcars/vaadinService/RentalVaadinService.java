778ff6364e301f03f2371c424a498d1e3b651c50
wnusiek <120226590+wnusiek@users.noreply.github.com>
2024-05-09 00:14:57 +0200

    Update RentalVaadinService.java

--- src/main/java/com/example/rentalcars/vaadinService/RentalVaadinService.java
+++ src/main/java/com/example/rentalcars/vaadinService/RentalVaadinService.java
@@ -9,48 +9,19 @@ import com.example.rentalcars.repository.*;
 import com.example.rentalcars.service.CarService;
 import com.example.rentalcars.service.DepartmentService;
 import com.example.rentalcars.service.ReservationService;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 import java.time.LocalDate;
 import java.util.List;
 
 @Service
+@RequiredArgsConstructor
 public class RentalVaadinService {
-
-    private final CarRepository carRepository;
-
     private final CarsBySpecificationService carsBySpecificationService;
     private final CarService carService;
     private final DepartmentService departmentService;
-    private final CompanyRepository companyRepository;
-    private final CustomerRepository customerRepository;
-    private final DepartmentRepository departmentRepository;
-    private final EmployeeRepository employeeRepository;
-    private final RentalRepository rentalRepository;
-    private final ReservationRepository reservationRepository;
-    private final ReturnRepository returnRepository;
-    private final CarsByDateService carsBydateService;
     private final ReservationService reservationService;
-
-    public RentalVaadinService(CarRepository carRepository, CarsBySpecificationService carsBySpecificationService, CarService carService, DepartmentService departmentService, CompanyRepository companyRepository,
-                               CustomerRepository customerRepository, DepartmentRepository departmentRepository,
-                               EmployeeRepository employeeRepository, RentalRepository rentalRepository,
-                               ReservationRepository reservationRepository, ReturnRepository returnRepository, CarsByDateService carsBydateService, ReservationService reservationService) {
-
-        this.carRepository = carRepository;
-        this.carsBySpecificationService = carsBySpecificationService;
-        this.carService = carService;
-        this.departmentService = departmentService;
-        this.companyRepository = companyRepository;
-        this.customerRepository = customerRepository;
-        this.departmentRepository = departmentRepository;
-        this.employeeRepository = employeeRepository;
-        this.rentalRepository = rentalRepository;
-        this.reservationRepository = reservationRepository;
-        this.returnRepository = returnRepository;
-        this.carsBydateService = carsBydateService;
-        this.reservationService = reservationService;
-    }
 
     public List<CarDto> findCarsByMark(String filterText) {
         if (filterText == null || filterText.isEmpty()) {
