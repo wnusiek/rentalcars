8db8d1be33c41b772c64f2b915ffbb23aac32a60
wnusiek <120226590+wnusiek@users.noreply.github.com>
2024-05-17 13:48:03 +0200

    Create DepartmentAdditionException.java

---
+++ src/main/java/com/example/rentalcars/Exceptions/DepartmentAdditionException.java
@@ -0,0 +1,7 @@
+package com.example.rentalcars.Exceptions;
+
+public class DepartmentAdditionException extends RuntimeException {
+    public DepartmentAdditionException(String message, Throwable cause) {
+        super(message, cause);
+    }
+}
