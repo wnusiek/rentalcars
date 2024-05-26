2e4a460cd34aa82362e6d448202281428e0f0812
wnusiek <120226590+wnusiek@users.noreply.github.com>
2024-05-26 21:48:09 +0200

    Create RentalAdditionException.java

---
+++ src/main/java/com/example/rentalcars/Exceptions/RentalAdditionException.java
@@ -0,0 +1,7 @@
+package com.example.rentalcars.Exceptions;
+
+public class RentalAdditionException extends RuntimeException {
+    public RentalAdditionException(String message, Throwable cause) {
+        super(message, cause);
+    }
+}
