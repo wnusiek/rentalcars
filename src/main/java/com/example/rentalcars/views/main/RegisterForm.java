b27288905e76d2f688721424813d1f893b8bd743
wnusiek <120226590+wnusiek@users.noreply.github.com>
2024-06-02 20:42:25 +0200

    Update RegisterForm.java

--- src/main/java/com/example/rentalcars/views/main/RegisterForm.java
+++ src/main/java/com/example/rentalcars/views/main/RegisterForm.java
@@ -1,44 +1,29 @@
 package com.example.rentalcars.views.main;
 
-import com.example.rentalcars.model.CustomerModel;
-import com.example.rentalcars.model.EmployeeModel;
 import com.example.rentalcars.model.UserModel;
 import com.vaadin.flow.component.Component;
 import com.vaadin.flow.component.ComponentEvent;
 import com.vaadin.flow.component.ComponentEventListener;
-import com.vaadin.flow.component.Text;
 import com.vaadin.flow.component.button.Button;
 import com.vaadin.flow.component.button.ButtonVariant;
 import com.vaadin.flow.component.formlayout.FormLayout;
 import com.vaadin.flow.component.orderedlayout.HorizontalLayout;
 import com.vaadin.flow.component.textfield.EmailField;
 import com.vaadin.flow.component.textfield.PasswordField;
+import com.vaadin.flow.component.textfield.TextField;
 import com.vaadin.flow.data.binder.BeanValidationBinder;
 import com.vaadin.flow.data.binder.Binder;
-import com.vaadin.flow.component.textfield.TextField;
 import com.vaadin.flow.data.binder.ValidationException;
 import com.vaadin.flow.data.validator.EmailValidator;
 import com.vaadin.flow.shared.Registration;
-import jakarta.validation.constraints.Email;
-import org.apache.catalina.User;
-
-
-import java.awt.*;
 
 public class RegisterForm extends FormLayout {
-
     Binder<UserModel> binder = new BeanValidationBinder<>(UserModel.class);
-
     TextField name = new TextField("name");
-
     PasswordField password= new PasswordField("password");
-
     EmailField email = new EmailField("email");
-
     Button save = new Button("Register");
-
     private UserModel userModel;
-
 
     public RegisterForm() {
         binder.bindInstanceFields(this);
@@ -60,9 +45,7 @@ public class RegisterForm extends FormLayout {
     private Component registerButtonLayout() {
         save.addThemeVariants(ButtonVariant.LUMO_PRIMARY);
         save.addClickListener(event -> validateAndSave());
-
         return new HorizontalLayout(save);
-
     }
 
     private void validateAndSave() {
@@ -76,7 +59,6 @@ public class RegisterForm extends FormLayout {
 
 
 public static abstract class ReqisterFormEvent extends ComponentEvent<RegisterForm> {
-
     private UserModel userModel;
 
     protected ReqisterFormEvent(RegisterForm source, UserModel userModel) {
