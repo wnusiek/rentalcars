80e29485b075c1d5dfd8b479c01c9fb0008e1322
wnusiek <120226590+wnusiek@users.noreply.github.com>
2024-06-04 23:04:57 +0200

    Update Reservation2View.java
    - Dodanie wyświetlania zdjęć samochodów
    - Formatowanie tekstu za pomocą styli

--- src/main/java/com/example/rentalcars/views/main/customer/Reservation2View.java
+++ src/main/java/com/example/rentalcars/views/main/customer/Reservation2View.java
@@ -6,6 +6,7 @@ import com.example.rentalcars.views.main.MainLayout;
 import com.vaadin.flow.component.Component;
 import com.vaadin.flow.component.Text;
 import com.vaadin.flow.component.avatar.Avatar;
+import com.vaadin.flow.component.dependency.CssImport;
 import com.vaadin.flow.component.html.Div;
 import com.vaadin.flow.component.orderedlayout.HorizontalLayout;
 import com.vaadin.flow.component.orderedlayout.VerticalLayout;
@@ -20,6 +21,7 @@ import java.util.List;
 @Route(value = "reservation2View", layout = MainLayout.class)
 @PageTitle("Rezerwowanie")
 @AnonymousAllowed
+@CssImport(value = "./styles/styles.css")
 public class Reservation2View extends Div {
     private final CarService carService;
 
@@ -29,6 +31,7 @@ public class Reservation2View extends Div {
         VirtualList<CarModel> list = new VirtualList<>();
         list.setItems(cars);
         list.setRenderer(carRenderer);
+        list.getElement().getStyle().set("height", "800px");
         add(list);
     }
 
@@ -36,19 +39,23 @@ public class Reservation2View extends Div {
             car -> {
                 HorizontalLayout cardLayout = new HorizontalLayout();
                 cardLayout.setMargin(true);
-                Avatar avatar = new Avatar(car.getCarInfo());
-                avatar.setHeight("64px");
-                avatar.setWidth("64px");
+                Avatar avatar = new Avatar();
+                String carImagesPath = "cars/";
+                avatar.setImage(carImagesPath + car.getPictureUrl());
+                avatar.setHeight("200px");
+                avatar.setWidth("200px");
                 VerticalLayout infoLayout = new VerticalLayout();
                 infoLayout.setSpacing(false);
                 infoLayout.setPadding(false);
-                infoLayout.add(new Div(new Text(car.getMark())));
-                infoLayout.add(new Div(new Text(car.getModel())));
-                infoLayout.add(new Div(new Text(car.getColor())));
-                infoLayout.add(new Div(new Text(car.getPrice().toString())));
-                infoLayout.add(new Div(new Text(car.getBody().toString())));
-                infoLayout.add(new Div(new Text(car.getAvailability().toString())));
-                cardLayout.add(infoLayout);
+
+                Div markAndModelDiv = new Div(new Text(car.getMark() + " " + car.getModel()));
+                markAndModelDiv.addClassName("large-text");
+                infoLayout.add(markAndModelDiv);
+                infoLayout.add(new Div(new Text("Cena:\t" + car.getPrice().toString())));
+                infoLayout.add(new Div(new Text("Nadwozie:\t" + car.getBody().toString())));
+                infoLayout.add(new Div(new Text("Skrzynia biegów:\t" + car.getGearbox().toString())));
+                infoLayout.add(new Div(new Text("Dostępność:\t" + car.getAvailability().toString())));
+                cardLayout.add(avatar, infoLayout);
                 return cardLayout;
             });
 
